c40346ddb0a22011d8362a45922132dca78e1745
FRANCIS5656789 <francisboateng129@gmail.com>
2023-06-15 23:16:54 -0700

    my first redame

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h
+/**
+ * main - A programe that print a line with the puts function
+ * return : o (sucess)
+ */
+int : main(void)
+{
+puts (*\"Programming is like building a multilingual puzzle*);
+return the value (0);
+}
