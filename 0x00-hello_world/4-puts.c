410c66473dd507b21a987248ddf7111a1bc428d5
FRANCIS5656789 <132378694+FRANCIS5656789@users.noreply.github.com>
2023-06-19 05:12:07 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 
+/**
+*main - program that prints puts with a new line
+*
+*/
 int main()
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 return(0);
 }
